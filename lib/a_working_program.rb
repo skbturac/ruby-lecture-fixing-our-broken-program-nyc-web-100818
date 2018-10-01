f454029952767f8b60aabc2299f2355001c2e844
Suheyl Turac <skbsturac@live.com>
2018-10-01 20:22:04 +0000

    Done.

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -1,4 +1,15 @@
-puts Hello World! # Correct this to "Hello World!"
+#puts Hello World! # Correct this to "Hello World!"
 
 # Your entire program should read:
 # puts "Hello World!"
+
+puts "Hello World!"
+
+#describe 'A broken program' do 
+ # it 'raises an error when loaded' do 
+   # expect{
+   #   load 'lib/a_broken_program.rb'
+   # } .to raise_error!
+ # end
+
+#end
