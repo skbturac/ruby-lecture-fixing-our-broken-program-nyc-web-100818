0af51478e91f579765e287187aa895f8b3cfceac
Suheyl Turac <skbsturac@live.com>
2018-10-01 20:13:27 +0000

    automatically backed up by learn

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -2,3 +2,12 @@ puts Hello World! # Correct this to "Hello World!"
 
 # Your entire program should read:
 # puts "Hello World!"
+
+describe 'A broken program' do 
+  it 'raises an error when loaded' do 
+    expect{
+      load 'lib/a_broken_program.rb'
+    } .to raise_error!
+  end
+
+end
