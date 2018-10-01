f016587029343d015b752bb02c1c041edb47c9eb
Suheyl Turac <skbsturac@live.com>
2018-10-01 20:21:04 +0000

    automatically backed up by learn

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -6,11 +6,11 @@
 puts "Hello World!"
 end
 
-describe 'A broken program' do 
-  it 'raises an error when loaded' do 
-    expect{
-      load 'lib/a_broken_program.rb'
-    } .to raise_error!
-  end
+#describe 'A broken program' do 
+ # it 'raises an error when loaded' do 
+   # expect{
+   #   load 'lib/a_broken_program.rb'
+   # } .to raise_error!
+ # end
 
-end
+#end
